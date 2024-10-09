a34c5cda2719e3573c1f5f9385c29491edc5f819
Ann Root <ann.root@bvsd.org>
2024-10-09 13:59:09 -0600

    Template for assignment

---
+++ loopdraw.py
@@ -0,0 +1,14 @@
+import py5
+import py5_tools
+
+def setup():
+    py5.size(700, 700)  # screen size
+    py5.rect_mode(py5.CENTER)  # rectangle mode is center
+    py5.background(20, 30, 40) #red green blue
+
+    pass
+
+def draw():
+    pass
+
+py5.run_sketch() # don't delete
